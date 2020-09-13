f6b43dec4ef04fa04a21fe030fa275070bc8a94b
wenner <nelsondiasdemedeiros@gmail.com>
2020-09-13 14:21:13 -0300

    feat: created Layout Component

---
+++ src/components/common/Layout/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './layout.css';
+import './styles.css';
+
+import { Wrapper } from './styles';
+
+const Layout = ({ children }) => {
+  return (
+    <Wrapper>
+      children
+    </Wrapper>
+  )
+}
+
+export default Layout;
