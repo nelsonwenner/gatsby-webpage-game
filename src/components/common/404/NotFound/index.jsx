58df0b4fbba9c4e0643fa245b9881a5398e4bbce
wenner <nelsondiasdemedeiros@gmail.com>
2020-09-14 16:52:19 -0300

    fix: bug lib styles

--- src/components/common/404/NotFound/index.jsx
+++ src/components/common/404/NotFound/index.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import { Container } from 'react-system-grid';
-import { Center } from './styles'
 
 export default () => (
-  <Center textAlign="center" pt={6} pb={6} as={Container}>
+  <Container>
     <h1>404 PAGE NOT FOUND</h1>
     <Link to="/">Go Back Home</Link>
-  </Center>
+  </Container>
+ 
 )
