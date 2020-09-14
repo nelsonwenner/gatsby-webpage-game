5612ac60f97848a0d58de48323055abc242930f7
wenner <nelsondiasdemedeiros@gmail.com>
2020-09-14 17:12:56 -0300

    fix: resolved bug in font

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 import Layout from '../components/common/Layout';
+import { SEO } from '../components/common/SEO';
 
 export default () => (
   <Layout> 
+    <SEO 
+      title="Breaking Artificial Society"
+      type="webpage"
+      author="Nelson Wenner, Felipe Gabriel"
+      description="Breaking artificial society game"
+    />
   </Layout>
 )
