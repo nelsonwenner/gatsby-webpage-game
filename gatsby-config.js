b4c72fc1af762fa817ce61d7ca9c655dc28456b1
wenner <nelsondiasdemedeiros@gmail.com>
2020-09-14 22:37:04 -0300

    wip: working in prodution

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,26 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+const config = require('./src/data/config');
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: config.defaultTitle,
+    description: config.defaultDescription,
+    author: config.author,
+  },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: config.defaultTitle,
+        short_name: 'starter',
+        start_url: '/',
+        background_color: "#fff",
+        theme_color: "#fff",
+        display: 'minimal-ui',
+        icon: './static/logo.svg',
+      },
+    },
+    'gatsby-plugin-offline',
+  ],
 }
--- src/components/common/404/NotFound/index.jsx
+++ src/components/common/404/NotFound/index.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import { Container } from 'react-system-grid';
+import { SEO } from '../../SEO';
 
 export default () => (
   <Container>
+    <SEO title="404: Not found" location="/404" />
     <h1>404 PAGE NOT FOUND</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     <Link to="/">Go Back Home</Link>
   </Container>
  
